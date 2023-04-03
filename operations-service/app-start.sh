29037431aab59b0fa88e59339835515cebc18558
vishIGOR <vish_ig3@mail.ru>
2023-04-03 22:35:50 +0700

    work with ws

--- operations-service/app-start.sh
+++ operations-service/app-start.sh
@@ -4,6 +4,10 @@ cp -r ./../nest-shared ./
 
 docker-compose up -d --build
 
+sleep 10
+
+docker exec core-web-1 node /home/node/project/dist/project/src/main
+
 sleep 3
 
 echo "Operations-service is started"
